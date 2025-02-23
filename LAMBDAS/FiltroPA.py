a536a6ffcfd50c2191b0d221454ea665c3020a8e
CSantiagoMartinez <cristian.martinez-g@mail.escuelaing.edu.co>
2025-02-23 17:04:06 -0500

    Filtro pasa altas

---
+++ LAMBDAS/FiltroPA.py
@@ -0,0 +1,42 @@
+
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+def TF(señal):
+                            #FUNCION LAMBDA DE TRANSFORMADA DE FOURIER 
+    N = len(señal)
+    valores = np.zeros(N, dtype=complex)
+    for k in range(N):
+        for n in range(N):
+            valores[k] += señal[n] * np.exp(-2j * np.pi * k * n / N)
+    freq = np.fft.fftfreq(N, d=1/1000)  # Frecuencias reales en Hz
+    return freq, valores
+
+def IFT(valores):
+                                ##transformada inversa de fourier (LAMBDA)
+    N = len(valores)
+    señal_reconstruida = np.zeros(N, dtype=complex)
+    for n in range(N):
+        for k in range(N):
+            señal_reconstruida[n] += valores[k] * np.exp(2j * np.pi * k * n / N)  # SIGNO CORREGIDO
+        señal_reconstruida[n] /= N  # Normalización
+    return señal_reconstruida.real  # Solo parte real
+
+
+
+
+def pasaltas(signal, cutoff, sampling_rate=1000):
+    ####FILTRO PASA ALTAS##
+    N = len(signal)
+    frequencies, valores = TF(signal)  # Obtener la DFT
+    
+    for k in range(N):
+        if abs(frequencies[k]) < cutoff:
+            valores[k] = 0  # ELIMINA FRECUENCIAS BAJAS
+
+    filtered_signal = IFT(valores)  
+    return filtered_signal, valores, frequencies
+
+
+
